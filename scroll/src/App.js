cb090f0b386ba61de5101f0f3108ddb61103a1ec
Daniel Kim <kkdh1109@gmail.com>
2021-12-20 12:48:20 -0500

    delaying effect and background change effect

--- scroll/src/App.js
+++ scroll/src/App.js
@@ -1,10 +1,45 @@
 import "./App.css";
 import React, { useRef, useEffect, useState } from "react";
+import Second from "./Second";
+import { gsap } from "gsap";
 
 function App() {
+  const headerRef = useRef(null);
   const ref = useRef();
+  const [background, setBackground] = useState("black");
 
-  return <div className="app"></div>;
+  const toggleBackground = () => {
+    const color = background !== "black" ? "blue" : "green";
+    setBackground(color);
+  };
+
+  useEffect(() => {
+    gsap.to(headerRef.current, {
+      duration: 1,
+      background: background,
+      ese: "none",
+    });
+  }, [background]);
+
+  useEffect(() => {
+    console.log(headerRef);
+    gsap.from(headerRef.current, {
+      duration: 1,
+      autoAlpha: 0,
+      ease: "none",
+      delay: 1,
+    });
+  }, [headerRef]);
+
+  return (
+    <div className="app">
+      <div className="app__first" ref={headerRef}>
+        <button onClick={toggleBackground}>toggle background</button>
+      </div>
+      <Second />
+      <div className="app__third"></div>
+    </div>
+  );
 }
 
 export default App;
