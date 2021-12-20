6e481d749bbd8fb39dec25978f092d0759bb8659
Daniel Kim <kkdh1109@gmail.com>
2021-12-20 17:38:05 -0500

    image comparison using gsap timeline

--- scroll/src/App.js
+++ scroll/src/App.js
@@ -1,11 +1,30 @@
 import "./App.css";
 import React, { useRef, useEffect, useState } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Second from "./Second";
 import { gsap } from "gsap";
 
+gsap.registerPlugin(ScrollTrigger);
+
+const sections = [
+  {
+    title: "title 1",
+    subtitle: "sub 1",
+  },
+  {
+    title: "title 2",
+    subtitle: "sub 2",
+  },
+  {
+    title: "title 3",
+    subtitle: "sub 3",
+  },
+];
+
 function App() {
   const headerRef = useRef(null);
-  const ref = useRef();
+  const revealsRefs = useRef([]);
+  revealsRefs.current = [];
   const [background, setBackground] = useState("black");
 
   const toggleBackground = () => {
@@ -13,31 +32,71 @@ function App() {
     setBackground(color);
   };
 
-  useEffect(() => {
-    gsap.to(headerRef.current, {
-      duration: 1,
-      background: background,
-      ese: "none",
-    });
-  }, [background]);
+  // useEffect(() => {
+  //   gsap.to(headerRef.current, {
+  //     duration: 1,
+  //     background: background,
+  //     ese: "none",
+  //   });
+  // }, [background]);
 
   useEffect(() => {
     console.log(headerRef);
+    console.log(headerRef.current);
+  }, [headerRef]);
+
+  useEffect(() => {
     gsap.from(headerRef.current, {
       duration: 1,
       autoAlpha: 0,
       ease: "none",
       delay: 1,
     });
-  }, [headerRef]);
+
+    revealsRefs.current.forEach((el, i) => {
+      gsap.fromTo(
+        el,
+        { autoAlpha: 0 },
+        {
+          duration: 1,
+          autoAlpha: 1,
+          ease: "none",
+          scrollTrigger: {
+            id: `section-${i + 1}`,
+            trigger: el,
+            start: "top center+=100",
+            toggleActions: "play none none reverse",
+            marks: true,
+          },
+        }
+      );
+    });
+  }, []);
+
+  const addToRefs = (el) => {
+    // console.log(el);
+    if (el && !revealsRefs.current.includes(el)) {
+      revealsRefs.current.push(el);
+    }
+    // console.log(revealsRefs.current);
+  };
 
   return (
     <div className="app">
-      <div className="app__first" ref={headerRef}>
+      <div className="app__first">
         <button onClick={toggleBackground}>toggle background</button>
       </div>
-      <Second />
-      <div className="app__third"></div>
+      <Second ref={headerRef} />
+      <div className="app__third">
+        {sections.map(({ title, subtitle }) => {
+          return (
+            <div key={title} className="app__section" ref={addToRefs}>
+              <h2>{title}</h2>
+              <p>{subtitle}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- scroll/src/Second.js
+++ scroll/src/Second.js
@@ -1,27 +1,57 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Second.css";
+import { gsap } from "gsap";
 
-export default function Second(inViewport) {
+export default function Second() {
+  useEffect(() => {
+    gsap.utils.toArray(".second__imgContainer").forEach((section) => {
+      let tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: section,
+          pinSpacer: false,
+          start: "20% 60%",
+          // makes the height of the scrolling (while pinning) match the width, thus the speed remains constant (vertical/horizontal)
+          end: () => "bottom 60%",
+          // end: () => "+=" + section.offsetWidth,
+          scrub: true,
+          // pin: true,
+          anticipatePin: 1,
+        },
+        defaults: { ease: "none" },
+      });
+
+      // animate the container one way...
+      tl.fromTo(
+        section.querySelector(".afterImage"),
+        { xPercent: 100, x: 0 },
+        { xPercent: 0 }
+      );
+      // ...and the image the opposite way (at the same time)
+      tl.fromTo(
+        section.querySelector(".afterImage img"),
+        { xPercent: -100, x: 0 },
+        { xPercent: 0 },
+        0
+      );
+
+      tl.fromTo(
+        section.querySelector(".beforeImage img"),
+        { autoAlpha: 1 },
+        { autoAlpha: 0 }
+      );
+    });
+  });
+
   return (
     <div className="second">
-      {/* <img className="second__1" src="./Toki_Desktop_Header.svg" alt="img" /> */}
-      <div></div>
-      <div className="second__imgContainer">
-        <img
-          className="second__img"
-          src="./Toki_Desktop_Header.svg"
-          alt="img"
-        />
-        <img
-          className="second__img"
-          src="./Toki_Desktop_Header.svg"
-          alt="img"
-        />
-        <img
-          className="second__img"
-          src="./Toki_Desktop_Header.svg"
-          alt="img"
-        />
+      <div className="second__imgContainer ">
+        <div className="second__comparisonImg  beforeImage">
+          <img className="second__img" src="./Benefit_1-1.svg" alt="img" />
+        </div>
+
+        <div className="second__comparisonImg  afterImage">
+          <img className="second__img" src="./Benefit_1-2.svg" alt="img" />
+        </div>
       </div>
       <div className="second__txts">
         <h1 className="second__h1">hi</h1>
