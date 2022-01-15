5a7fa6baf3a31249864a5273c0ae868f945f34cc
Daniel Kim <kkdh1109@gmail.com>
2022-01-15 17:03:45 -0500

    use splice method to remove element from the arr

--- leetcode/removeElement.js
+++ leetcode/removeElement.js
@@ -37,3 +37,14 @@
 // Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
 // Note that the five elements can be returned in any order.
 // It does not matter what you leave beyond the returned k (hence they are underscores).
+
+//can splice the arr when val is present
+var removeElement = function (nums, val) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return nums.length;
+};
